ebda92d07d3911f4f1cd172d21d25b069f29857f
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-11 20:44:10 +0700

    (fix): tsconfig path: components -> components/*

--- src/components/page-intro.svelte
+++ src/components/page-intro.svelte
@@ -2,7 +2,6 @@
   export let heading: string;
   export let subheading: Optional<string> = undefined;
   export let tagline: Optional<string> = undefined;
-  export let animate = true;
 </script>
 
 <section>
