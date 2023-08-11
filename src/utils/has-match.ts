10f7e06f0f246bed84543ba3ae7b0b5201073ec2
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-11 22:21:04 +0700

    (feat): add eslint, fix hasMatch generic

--- src/utils/has-match.ts
+++ src/utils/has-match.ts
@@ -1,35 +1,39 @@
-export default function hasMatch<T extends Record<string, any>>(source: T | Record<string, unknown>, search: string, includeKeys?: string[]): boolean {
-  const normalizedSearch = search.normalize().toLowerCase()
+export default function hasMatch<T extends Record<string, unknown>>(
+  source: T,
+  search: string,
+  includeKeys?: string[],
+): boolean {
+  const normalizedSearch = search.normalize().toLowerCase();
 
   const result = Object.keys(source).map((key) => {
-    const value = source[key]
+    const value = source[key];
 
     if (!!includeKeys && includeKeys.length > 0 && !includeKeys.includes(key)) {
-      return false
+      return false;
     }
 
-    if (typeof value === 'string') {
-      const normalizedValue = source[key].normalize().toLowerCase()
+    if (typeof value === "string") {
+      const normalizedValue = value.normalize().toLowerCase();
 
-      return normalizedValue.includes(normalizedSearch)
+      return normalizedValue.includes(normalizedSearch);
     }
 
     if (Array.isArray(value)) {
       const result = value.map((item) => {
-        if (typeof item === 'string') {
-          const normalizedValue = item.normalize().toLowerCase()
+        if (typeof item === "string") {
+          const normalizedValue = item.normalize().toLowerCase();
 
-          return normalizedValue.includes(normalizedSearch)
+          return normalizedValue.includes(normalizedSearch);
         }
 
-        return false
-      })
+        return false;
+      });
 
-      return result.includes(true)
+      return result.includes(true);
     }
 
-    return false
-  })
+    return false;
+  });
 
-  return result.includes(true)
+  return result.includes(true);
 }
