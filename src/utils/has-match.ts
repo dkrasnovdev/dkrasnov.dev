11c1aaca501e6ae6d13ac8292eb2cff6b1e90743
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-11 22:07:54 +0700

    (feat): add has-match utility

---
+++ src/utils/has-match.ts
@@ -0,0 +1,35 @@
+export default function hasMatch<T extends Record<string, any>>(source: T | Record<string, unknown>, search: string, includeKeys?: string[]): boolean {
+  const normalizedSearch = search.normalize().toLowerCase()
+
+  const result = Object.keys(source).map((key) => {
+    const value = source[key]
+
+    if (!!includeKeys && includeKeys.length > 0 && !includeKeys.includes(key)) {
+      return false
+    }
+
+    if (typeof value === 'string') {
+      const normalizedValue = source[key].normalize().toLowerCase()
+
+      return normalizedValue.includes(normalizedSearch)
+    }
+
+    if (Array.isArray(value)) {
+      const result = value.map((item) => {
+        if (typeof item === 'string') {
+          const normalizedValue = item.normalize().toLowerCase()
+
+          return normalizedValue.includes(normalizedSearch)
+        }
+
+        return false
+      })
+
+      return result.includes(true)
+    }
+
+    return false
+  })
+
+  return result.includes(true)
+}
