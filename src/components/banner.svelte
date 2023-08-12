76616601c260eb837900beafbccf0368a57103ba
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-12 14:01:33 +0700

    (feat): add banner

---
+++ src/components/banner.svelte
@@ -0,0 +1,7 @@
+<aside
+  class="flex h-banner-height items-center overflow-x-hidden whitespace-nowrap px-2.5 text-center"
+>
+  <div class="flex-1" />
+  <p>Work In Progress</p>
+  <div class="flex-1" />
+</aside>
