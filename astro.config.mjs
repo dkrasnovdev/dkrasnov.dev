235d3f8c7bcf2cc844f5068a3af2fe551b73d625
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-12 19:53:38 +0700

    (feat): add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import svelte from "@astrojs/svelte";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dkrasnov.dev",
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
-  integrations: [tailwind(), mdx(), svelte()],
+  integrations: [tailwind(), mdx(), svelte(), react()]
 });
