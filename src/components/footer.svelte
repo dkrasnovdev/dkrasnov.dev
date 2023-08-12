df22d058b364c6e3e94bc8751ec1aa54fc3972a7
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-08-12 17:40:38 +0700

    (feat): re-style footer nav

--- src/components/footer.svelte
+++ src/components/footer.svelte
@@ -7,19 +7,19 @@
 </script>
 
 <footer
-  class="item-center flex h-footer-height justify-center border-t-2 border-neutral-800 px-page-gutter"
+  class="item-center flex h-footer-height justify-center border-t border-neutral-800 px-page-gutter"
 >
   <div
-    class="flex w-full max-w-content flex-col-reverse items-center justify-between gap-3 sm:flex-row sm:gap-0"
+    class="flex w-full max-w-content flex-col-reverse items-center justify-center gap-3 sm:flex-row sm:justify-between sm:gap-0"
   >
-    <p class="font-semibold text-neutral-500">dmitrykrasnov.</p>
+    <p class="text-sm font-semibold text-neutral-600">dmitrykrasnov</p>
     <nav aria-label="Socials">
       <ul role="list" class="flex items-center">
         {#each menu as [name, href]}
           <li>
             <a
               {href}
-              class="rounded-md px-2.5 py-1.5 font-semibold text-neutral-500 transition hover:text-neutral-200 focus:outline-none focus:ring focus:ring-inset"
+              class="rounded-md px-2.5 py-1.5 text-sm font-semibold text-neutral-500 transition hover:text-neutral-200 focus:outline-none focus:ring focus:ring-inset"
             >
               {name}
             </a>
