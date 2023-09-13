322f0d1a51a52e5dc9c85cbf40320e8cbd1a6135
Dmtitry Krasnov <dkrasnov.dev@gmail.com>
2023-09-13 21:22:37 +1200

    (feat): gitignore .vercel output

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client-image" />
-/// <reference types="astro/client-image" />
+/// <reference types="astro/client" />
+/// <reference types="astro/client" />
 
 // Utility type primarily for optional props in Svelte because the `prop:?`
 type Optional<T> = T | null | undefined;
